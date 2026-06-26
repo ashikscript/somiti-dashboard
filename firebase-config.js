58523989786e1aeb6a78c9287136fd68ce8a7296
Ashikur Rahman <xashikx170@gmail.com>
2026-06-26 14:46:22 +0600

    Update Firebase configuration with new values

--- firebase-config.js
+++ firebase-config.js
@@ -15,3 +15,21 @@ export const firebaseConfig = {
   messagingSenderId: "REPLACE_WITH_SENDER_ID",
   appId: "REPLACE_WITH_APP_ID"
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCebewYZloYRu0IX0Daw3qg7nwTUL1if3M",
+  authDomain: "our-somiti.firebaseapp.com",
+  projectId: "our-somiti",
+  storageBucket: "our-somiti.firebasestorage.app",
+  messagingSenderId: "527957710804",
+  appId: "1:527957710804:web:68fa91fbf1e2126560e841"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
